c6cf072bde20f75700ce32008bef434a507112b7
AdminLab <rsantoso@utsjr.edu.mx>
2023-06-29 08:48:12 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express=require("express");
+var usuarios=require("./rutas/usuarios");
+var path=require("path");
+var session=require("express-session");
+require("dotenv").config();
+
+var app=express();
+app.set("view engine", "ejs");
+app.use("/web", express.static(path.join(__dirname,"/web")));
+app.use(express.urlencoded({extended:true}));
+app.use(session({
+    secret: process.env.SECRETO_SESSION,
+    resave:true,
+    saveUninitialized:true
+}));
+
+app.use("/",usuarios);
+
+var port= process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log(`Servidor en http://localhost:${port}`);
+});
